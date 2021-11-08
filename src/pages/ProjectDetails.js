5b9963e88e044759fcc5b4a18b51d42728728565
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2021-11-08 13:02:19 +0100

    removed unused var

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -13,7 +13,7 @@ import { pageAnime } from "../animation";
 const ProjectDetails = () => {
   const history = useHistory();
   const url = history.location.pathname;
-  const [movies, setMovies] = useState(MovieState);
+  const [movies] = useState(MovieState);
   const [movie, setMovie] = useState(null);
   useEffect(() => {
     const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
