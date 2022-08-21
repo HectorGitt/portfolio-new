4559ee9244bad43204dde88631e82b1930c2c09f
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-08-21 15:58:47 +0100

    added about me section

---
+++ src/components/DetailSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { LayoutStyle } from '../styles'
+
+const DetailSection = () => {
+  return (
+    <LayoutStyle>
+    <>i am a Software Engineer who is skilled in building Full-stack Web Applications with software technologies like ReactJs, Typescript and NextJs for frontend. Django, Flask and firebase for backend, with MySQL and Postgresql for database management. i seek an avenue to explore and grow and i am ready to execute tasks as instructed.
+    I am also actively learning more relevant tech skills to stay relevant in the industry.
+
+    I am a Software developer with 3 years of experience who has worked on a lots of Web Applications with HTML, CSS, JavaScript, SCSS, Bootstrap, Tailwind and Jquery alongside react libaries like, react router, framer motion, styled components, React-context, redux, Nextjs, Graphql , react intersection observer, API integration and firebase. i seek an avenue to explore and grow and i am also ready to execute tasks as instructed and also ready to pickup more relevant tech skills required.
+    </> 
+    </LayoutStyle>
+  )
+}
+
+export default DetailSection
