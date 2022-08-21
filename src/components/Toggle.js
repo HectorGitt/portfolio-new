8edaf3da3d716f1c31e7122edd7e7fdf376ed5c4
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-08-21 14:28:17 +0100

    accordion open by defaut

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,9 +2,9 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Toggle = ({ children, title }) => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
   return (
-    <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
+    <motion.div layout className="question" onClick={() => setToggle((toggle) => !toggle)}>
       <motion.h4 layout>{title}</motion.h4>
       {toggle ? children : ""}
       <div className="line"></div>
