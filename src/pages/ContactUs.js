05425d99152ce45bf471ec5c9eb5fc3e5f2d1063
Hector <adeniyi.olaitanhector@yahoo.com>
2022-10-18 02:01:50 +0100

    updated contact page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,6 +3,12 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { pageAnime, titleAnim } from "../animation";
 
+//icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+
 const ContactUs = () => {
   return (
     <ContactStyle
@@ -19,22 +25,34 @@ const ContactUs = () => {
       <div>
         <Hide>
           <Social variants={titleAnim}>
-            <Circle />
-            <h2>Send Us A Message</h2>
-          </Social>
-        </Hide>
-        <Hide>
-          <Social variants={titleAnim}>
-            <Link href="mailto:adeniyi.olaitanhector@yahoo.com">
               <Circle />
               <h2>Send An Email</h2>
-            </Link>
+              <span>
+              <a href="mailto:adeniyi.olaitanhector@yahoo.com">
+                <FontAwesomeIcon icon={faMessage} size="2x" />
+              </a>
+              </span>
           </Social>
         </Hide>
         <Hide>
           <Social variants={titleAnim}>
             <Circle />
-            <h2>Social Media</h2>
+            <h2>Connect with me</h2>
+            <Link>
+            <span>
+              <a
+                target="__blank"
+                href="https://www.linkedin.com/in/adeniyi-olaitan-b72100143/"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+            </span>
+            <span>
+              <a target="__blank" href="https://github.com/hectorGitt">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+            </span>
+            </Link>
           </Social>
         </Hide>
       </div>
@@ -66,12 +84,16 @@ const Social = styled(motion.div)`
   h2 {
     margin: 2rem;
   }
+  a {
+    color: #353535;
+  }
 `;
 const Link = styled.a`
   display: flex;
   align-items: center;
   text-decoration: none;
   color:inherit;
+  gap: 2rem;
 `;
 
 export default ContactUs;
